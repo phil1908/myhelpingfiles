f9f00a94354d6f553942626c8a59b704c54bfe94
phil1908 <38127970+phil1908@users.noreply.github.com>
2020-12-09 09:25:57 +0100

    Create LanguageSetup.ps1

---
+++ LanguageSetup.ps1
@@ -0,0 +1,29 @@
+# Script to define regional settings on Azure Virtual Machines deployed from the market place
+# Author: Alexandre Verkinderen
+# Blogpost: https://mscloud.be/configure-regional-settings-and-windows-locales-on-azure-virtual-machines/
+#
+######################################
+
+#variables
+$regionalsettingsURL = "https://raw.githubusercontent.com/phil1908/WVD-helpers/master/DERegion.xml"
+$RegionalSettings = ""C:\Packages\DERegion.xml""
+
+
+#downdload regional settings file
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+
+
+# Set Locale, language etc. 
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+
+# Set languages/culture. Not needed perse.
+Set-WinSystemLocale de-DE
+Set-WinUserLanguageList -LanguageList de-DE -Force
+Set-Culture -CultureInfo de-DE
+Set-WinHomeLocation -GeoId 94
+Set-TimeZone -Name "W. Europe Standard Time"
+
+# restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
+Start-sleep -Seconds 40
+Restart-Computer
