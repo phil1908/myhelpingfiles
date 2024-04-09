a3e166a591defead090e6c67acbfe7e12ee9dd50
phil1908 <38127970+phil1908@users.noreply.github.com>
2024-04-09 16:58:07 +0100

    Update TASK-de-de_Language.ps1

--- TASK-de-de_Language.ps1
+++ TASK-de-de_Language.ps1
@@ -2,22 +2,14 @@ $source = "https://intuneblobthephilip.blob.core.windows.net/openuri/de-de_Langu
 $destination = "c:\windows\temp\de-de_Language.ps1"
 
 if (!(Test-Path -Path $destination)) {
-  #Copy-Item -Path $source -Destination $destination
   Invoke-WebRequest -Uri $source -OutFile $destination
     
 } else {
   Write-Output "Destination file already exists"
 }
 
-## Create Schedueled Tasks for the pop-up notifications
-# Set the date to 09:00 on the day that the script runs
-#$scheduledDate = [DateTime]::Today.AddHours(9)
-#$scheduledDate = [DateTime]::Today.AddDays(30)
-# Define Scheduled Task for the notifications. (the time and the messages are defined in the 'M365InstallationNotifications.ps1' script) 
-#$notificationPrincipal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users" -RunLevel Limited
 $Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -RunLevel Highest
-#$Principal = New-ScheduledTaskPrincipal -UserID $user -RunLevel Highest
-$Actions = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-Windowstyle Hidden -ExecutionPolicy Bypass - File 'c:\windows\temp\de-de_Language.ps1'"
+$Actions = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-Windowstyle Hidden -ExecutionPolicy Bypass -File c:\windows\temp\de-de_Language.ps1'
 $Trigger = New-ScheduledTaskTrigger -AtStartup
 $Settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -Hidden -StartWhenAvailable
 $Task = New-ScheduledTask -Principal $Principal -Action $Actions -Trigger $Trigger -Settings $Settings
