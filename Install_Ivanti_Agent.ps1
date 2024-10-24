393646927c28ce450c46428578e2a0a9bc3d5ce2
phil1908 <38127970+phil1908@users.noreply.github.com>
2024-10-24 16:57:51 +0200

    Update Install_Ivanti_Agent.ps1

--- Install_Ivanti_Agent.ps1
+++ Install_Ivanti_Agent.ps1
@@ -2,4 +2,4 @@
 $url = "https://cestgaleriespro01.blob.core.windows.net/container-ivantiagents-01/carexpert%20AVD%20Client%20Agent%20Group%202021.1%20SU3.exe"
 
 # Installation direkt ausführen
-Start-Process -FilePath "powershell" -ArgumentList "-Command", "& {Invoke-WebRequest -Uri $url -OutFile $env:TEMP\carexpert_AVD_Client_Agent_Group_2021.1_SU3_with_status.exe; Start-Process -FilePath $env:TEMP\carexpert_AVD_Client_Agent_Group_2021.1_SU3.exe -Wait}"
+Start-Process -FilePath "powershell" -ArgumentList "-Command", "& {Invoke-WebRequest -Uri $url -OutFile $env:TEMP\carexpert_AVD_Client_Agent_Group_2021.1_SU3.exe; Start-Process -FilePath $env:TEMP\carexpert_AVD_Client_Agent_Group_2021.1_SU3.exe -Wait}"
